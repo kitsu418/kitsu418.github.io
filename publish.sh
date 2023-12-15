401e5ca1158fba3ffe7ac857a62e5960c4387077
Weiyi Chen <wychen325@gmail.com>
2023-12-15 17:59:14 +0800

    update

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find ./static/images/ -type f -exec file --mime-type {} \+ | awk -F: '{if ($2 ~/image\//) print $1}' | xargs -I mogrify -strip {}
+find ./static/images/ -type f -exec file --mime-type {} \+ | awk -F: '{if ($2 ~/image\//) print $1}' | xargs -i mogrify -strip {}
 git add -A -- .
 git commit -m "update"
